a250159de52ecdf2d0fd76eed90b1038a04d4a31
Daniil Markov <mrmrkda@gmail.com>
2021-07-24 12:58:24 +0700

    chore: refactor data

---
+++ src/data/index.ts
@@ -0,0 +1,5 @@
+import goals_logo from './goals_logo.json'
+import university_logo from './university_logo.json'
+import walls_logo from './walls_logo.json'
+
+export { goals_logo, university_logo, walls_logo }
