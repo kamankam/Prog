3c117a1ce5ea27e0534abbb8c006d95160e12994
kamankam <kamanovae@gmail.com>
2021-07-24 21:19:28 +0700

    add human_logo

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 import goals_logo from './goals_logo.json'
 import university_logo from './university_logo.json'
 import walls_logo from './walls_logo.json'
+import human_logo from './human_logo.json'
 
-export { goals_logo, university_logo, walls_logo }
+export { goals_logo, university_logo, walls_logo, human_logo }
