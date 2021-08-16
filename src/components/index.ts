9e7217cb89c4a1160187ae813d7fed5d256c8e13
Daniil Markov <mrmrkda@gmail.com>
2021-08-16 16:42:51 +0700

    chore: gallery refactoring

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,19 +1,21 @@
+import Business from './Business'
+import Gallery from './gallery/Gallery'
+import GalleryCard from './gallery/GalleryCard'
 import GoalsCard from './GoalsCard'
 import Header from './Header'
 import Main from './Main'
 import UniversityCard from './University'
 import WallCard from './WallCard'
 import Work from './Work'
-import GalleryCard from './GalleryCard'
-import Business from './Business'
 
 export {
+  Business,
+  Gallery,
+  GalleryCard,
   GoalsCard,
   Header,
   Main,
   UniversityCard,
   WallCard,
   Work,
-  GalleryCard,
-  Business,
 }
